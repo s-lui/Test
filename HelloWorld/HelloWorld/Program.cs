8b0992943e28436420c019393b7f0d091e2c8ae7
s-lui <43795842+s-lui@users.noreply.github.com>
2018-10-02 16:18:49 -0700

    Just testing
    A Hello World in C#

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string input = Console.ReadLine();
+
+            Console.WriteLine("The input given is: " + input);
+            Console.ReadLine();
+
+        }
+    }
+}
